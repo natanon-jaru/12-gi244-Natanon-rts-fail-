1e3a6df114bc1044ab57c3e3a20c8e180ec923f5
natanon-jaru <149050860+natanon-jaru@users.noreply.github.com>
2024-01-18 11:12:54 +0700

    zoom/Ui/Map/Rotation

---
+++ Assets/Scripts/MainUI.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainUI : MonoBehaviour
+{
+    public static MainUI instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
