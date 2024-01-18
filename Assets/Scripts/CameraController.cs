308af8627b089ade639ece7590ac4ae382940a56
natanon-jaru <149050860+natanon-jaru@users.noreply.github.com>
2024-01-18 10:10:07 +0700

    Camera Controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+
+    [Header("Move")] private float moveSpeed;
+    [SerializeField] private float xInput;
+    [SerializeField] private float zInput;
+
+    public static CameraController instance;
+
+    private void Awake()
+    {
+        instance = this;
+        cam = Camera.main;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveByKB();
+    }
+
+    void MoveByKB()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+
+        Vector3 dir = (transform.forward * zInput) + (transform.right * xInput);
+
+        transform.position += dir * moveSpeed * Time.deltaTime;
+        
+    }
+}
